c68ac268b49dc849241843c544cf9da21c66bff4
posix <uso529133@gmail.com>
2019-07-17 00:19:57 -0700

    :q

--- main.cpp
+++ main.cpp
@@ -3,12 +3,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-char *g_packet = "\x00\x30\x0d\xc2\x55\xed\x98\x2c\xbc\x6d\x42\x9b\x08\x00\x45\x00" \
-				"\x00\x28\xc0\x06\x40\x00\x80\x06\x00\x00\xc0\xa8\x19\x3b\x01\xe1" \
-				"\x23\x2d\xd3\x85\x01\xbb\x2c\x38\x8f\x24\xa3\xa5\x4e\xf7\x50\x10" \
-				"\x02\x02\xff\x0b\x00\x00\x12\x34\x56";
-
-
 struct eth_hdr {
 	uint8_t dst_mac[6];
 	uint8_t src_mac[6];
@@ -16,7 +10,8 @@ struct eth_hdr {
 };
 
 struct ipv4_hdr {
-	uint8_t version;
+	uint8_t header_len : 4;
+	uint8_t version : 4;
 	uint8_t type_of_service;
 	uint16_t total_packet_len;
 	uint16_t fragment_identification;
@@ -33,7 +28,8 @@ struct tcp_hdr {
 	uint16_t dst_port;
 	uint32_t seq_num;
 	uint32_t ack_num;
-	uint16_t flags;
+	uint8_t header_len : 4;
+	uint16_t flags : 12;
 	uint16_t window_size;
 	uint16_t checksum;
 	uint16_t urgent_ptr;
@@ -43,10 +39,6 @@ struct tcp_hdr {
 void usage() {
   printf("syntax: pcap_test <interface>\n");
   printf("sample: pcap_test wlan0\n");
-}
-
-bool check_type_1(eth_hdr *p) {
-	return p->type == htons(0x0800);
 }
 
 void print_mac(eth_hdr *p) {
@@ -60,9 +52,6 @@ void print_mac(eth_hdr *p) {
 void print_ip(ipv4_hdr *p) {
 	uint8_t *src_ip = p->src_ip;
 	uint8_t *dst_ip = p->dst_ip;
-
-	// uint8_t tmp[] = { 0x01, 0xe1, 0x23, 0x2d };
-	// src_ip = (uint8_t *)&tmp;
 
 	printf("source ip : %u.%u.%u.%u\n", src_ip[0], src_ip[1], src_ip[2], src_ip[3]);
 	printf("destination ip : %u.%u.%u.%u\n", dst_ip[0], dst_ip[1], dst_ip[2], dst_ip[3]);
@@ -79,9 +68,10 @@ void print_port(tcp_hdr *p) {
 void print_tcp_data(tcp_hdr *p, uint8_t len) {
 	uint8_t data = *p->data;
 
-	printf("data : \"");
+	printf("data(%d) : \"", len);
+	if (len > 10) len = 0;
 	for (uint8_t i = 0; i < len; ++i) {
-		printf("\\x%x", p->data[i]);
+		printf("\\x%02x", p->data[i]);
 	}
 	printf("\"\n");
 }
@@ -105,33 +95,31 @@ int main(int argc, char* argv[]) {
    	struct pcap_pkthdr* header;
    	const u_char* packet;
    	int res = pcap_next_ex(handle, &header, &packet);
-	// timeout
+
+	uint8_t eth_len;
+	uint8_t ipv4_len;
+	uint8_t tcp_len;
+
 	if (res == 0) continue;
-	// error / got EOF
    	if (res == -1 || res == -2) break;
-		printf("\n\n%u bytes captured\n", header->caplen);
-	
 		eth_hdr* eth_ptr = (eth_hdr*)packet;
-		print_mac(eth_ptr);
-
 		if (eth_ptr->type != 8) continue;
-
-		ipv4_hdr* ipv4_ptr = (ipv4_hdr*) (packet + 14);
-		print_ip(ipv4_ptr);
-
+		eth_len = 14;
+		ipv4_hdr* ipv4_ptr = (ipv4_hdr*) (packet + eth_len);
 		if (ipv4_ptr->protocol != 6) continue;
+		ipv4_len = ipv4_ptr->header_len * 4;
+		tcp_hdr* tcp_ptr = (tcp_hdr*) (packet + eth_len + ipv4_len);
+		tcp_len = (tcp_ptr->flags) * 4;
 
-		tcp_hdr* tcp_ptr = (tcp_hdr*) (packet + 34);
+		printf("\n\n%u bytes captured\n", header->caplen);
+		print_mac(eth_ptr);
+		print_ip(ipv4_ptr);
 		print_port(tcp_ptr);
-
-		uint8_t data_len = 54 - header->caplen;
-		if (data_len > 10) data_len = 10;
+		
+		uint8_t data_len = header->caplen - (eth_len + ipv4_len + tcp_len);
 
 		print_tcp_data(tcp_ptr, data_len);
-
-
 	}
-
 
 	pcap_close(handle);
 
